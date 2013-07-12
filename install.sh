c35ef9fac90d13d3a4ce12ff0efd0983b6b12263
evilsocket <evilsocket@gmail.com>
2013-07-12 02:05:05 +0200

    better install.sh script

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+#!/usr/bin/env bash
 
-########## Variables
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
-
-##########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
