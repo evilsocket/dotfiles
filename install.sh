3d9617feba9007d66f153fd82fab2cc6e73871d3
Simone Margaritelli <evilsocket@gmail.com>
2016-10-08 13:01:24 +0200

    Cloning bundle inside vim before copying folder.

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 files=( 'oh-my-zsh' 'android' 'bin' 'vim' 'vimrc' 'zshrc' 'tmux.conf' )
 
+git clone http://github.com/gmarik/vundle.git ./vim/bundle/vundle
+
 for file in "${files[@]}"
 do
     echo "Installing $file ..."
